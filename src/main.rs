2a70f30c60149df9843923d5a485ca9e29706b7c
syntaxbullet <syntaxbullet@protonmail.com>
2024-01-06 14:46:36 +0100

    chore: add cli command signatures

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,93 @@
 mod task;
-use task::Task;
+use clap::{Parser, Subcommand, Args};
+#[derive (Parser)]
+#[command(name="citrine", version="0.1.0")]
+#[command(author="syntaxbullet <syntaxbullet@protonmail.com>")]
+#[command(about="A simple task manager following the unix philosophy")]
+
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive (Subcommand)]
+
+enum Commands {
+    Add(Add),
+    Update(Update),
+    Delete(Delete),
+    List(List),
+}
+
+#[derive (Args)]
+/// Add a new task to the task list
+struct Add {
+    /// The title of the task
+    title: String,
+    /// The due date of the task in rfc3339 format (e.g. 2021-01-01T00:00:00+00:00) 
+    #[arg(short = 'd', long = "due")]
+    due_date: Option<String>,
+    /// The priority of the task [0-9] 
+    #[arg(short = 'p', long = "priority")]
+    priority: Option<u8>,
+    /// The tags of the task , must be a comma separated list
+    #[arg(short = 't', long = "tags")]
+    tags: Option<Vec<String>>,
+}
+#[derive (Args)]
+/// Update an existing task in the task list
+struct Update {
+    /// The id of the task to update
+    id: u32,
+    /// The due date of the task in rfc3339 format (e.g. 2021-01-01T00:00:00+00:00)
+    #[arg(short = 'd', long = "due")]
+    due_date: Option<String>,
+    /// The priority of the task (0-9)
+    #[arg(short = 'p', long = "priority")]
+    priority: Option<u8>,
+    /// The tags of the task, must be a comma separated list
+    #[arg(short = 't', long = "tags")]
+    tags: Option<Vec<String>>,
+    /// The status of the task
+    #[arg(short = 's', long = "status")]
+    status: Option<String>,
+    /// The title of the task
+    #[arg(short = 'm', long = "message")]
+    title: Option<String>,
+    /// remove tags from the task 
+    #[arg(short = 'r', long = "remove-tags")]
+    remove_tag: Option<String>,
+    /// append tags to the task 
+    #[arg(short = 'a', long = "append-tags")]
+    append_tag: Option<String>,
+}
+
+#[derive (Args)]
+/// Delete an existing task from the task list
+struct Delete {
+    /// The id of the task to remove
+    id: u32,
+}
+
+#[derive (Args)]
+/// List all tasks in the task list filtered by the given options
+struct List {
+    /// The due date of the task in rfc3339 format (e.g. 2021-01-01T00:00:00+00:00) 
+    #[arg(short = 'd', long = "due")]
+    due_date: Option<String>,
+    /// The priority of the task [0-9] 
+    #[arg(short = 'p', long = "priority")]
+    priority: Option<u8>,
+    /// The tags of the task , must be a comma separated list
+    #[arg(short = 't', long = "tags")]
+    tags: Option<Vec<String>>,
+    /// The status of the task
+    #[arg(short = 's', long = "status")]
+    status: Option<String>,
+    /// The title of the task
+    #[arg(short = 'm', long = "message")]
+    title: Option<String>,
+}
 fn main() {
-    // 1. [x] some task - due: 2020-02-20T12:00:00+09:00 priority: 1 tags: tag1,tag2
-    let some_task_string = "1. [>] some task - tags: tag1,tag2 due: 2020-02-20T12:00:00+09:00 priority: 1";
-    let task = Task::parse(some_task_string).unwrap();
-    println!("{:?}", Task::stringify(task));
+  let _cli = Cli::parse();
 } 
--- src/task.rs
+++ src/task.rs
@@ -1,108 +1,108 @@
-#[derive(Debug)]
-pub struct Task {
-    id: u32,
-    title: String,
-    status: Status,
-    due_date: Option<String>,
-    priority: Option<u8>,
-    tags: Option<Vec<String>>,
-}
+// #[derive(Debug)]
+// pub struct Task {
+//     id: u32,
+//     title: String,
+//     status: Status,
+//     due_date: Option<String>,
+//     priority: Option<u8>,
+//     tags: Option<Vec<String>>,
+// }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Status {
-    OPEN,
-    INPROGRESS,
-    DONE,
-    OVERDUE,
-}
-fn parse_from_string(input: &str) -> Option<Task> {
-    // 1. [x] some task - due: 2020-02-20T12:00:00+09:00 priority: 1 tags: tag1,tag2
-    let mut due_date: Option<String> = None;
-    let mut priority: Option<u8> = None;
-    let mut tags: Option<Vec<String>> = None;
-    let id: u32;
-    let status: Status;
-    let title: String;
+// #[derive(Debug, Clone, PartialEq)]
+// pub enum Status {
+//     OPEN,
+//     INPROGRESS,
+//     DONE,
+//     OVERDUE,
+// }
+// fn parse_from_string(input: &str) -> Option<Task> {
+//     // 1. [x] some task - due: 2020-02-20T12:00:00+09:00 priority: 1 tags: tag1,tag2
+//     let mut due_date: Option<String> = None;
+//     let mut priority: Option<u8> = None;
+//     let mut tags: Option<Vec<String>> = None;
+//     let id: u32;
+//     let status: Status;
+//     let title: String;
 
-    // to optain the id, we need to parse all characters until the first dot, and parse into u32
-    id = input.chars().take_while(|c| c != &'.').collect::<String>().parse::<u32>().unwrap();
-    // obtaining the status is easy, we just need to look for an [ and a ] and grab that, then we match it to a status depending on which char is inside
-    let openbracket = input.chars().position(|c| c == '[').unwrap();
-    // check if there is a closing bracket exactly 2 chars after the opening bracket
-    let closedbracket = input.chars().position(|c| c == ']').unwrap();
-    if closedbracket - openbracket == 2 {
-        status = match input.chars().nth(openbracket + 1).unwrap() {
-            'x' => Status::DONE,
-            ' ' => Status::OPEN,
-            '>' => Status::INPROGRESS,
-            '!' => Status::OVERDUE,
-            _ => panic!("Failed to parse Task: Invalid status"),
-        };
-    } else {
-        panic!("Failed to parse Task: Invalid status");
-    }
-    // obtaining the title is a bit more tricky, we need to find the first space after the closing bracket, and then take all characters until we encounter a dash or the string ends
-    title = input.chars().skip(closedbracket + 1).take_while(|c| c != &'-').collect::<String>().trim().to_string();
+//     // to optain the id, we need to parse all characters until the first dot, and parse into u32
+//     id = input.chars().take_while(|c| c != &'.').collect::<String>().parse::<u32>().unwrap();
+//     // obtaining the status is easy, we just need to look for an [ and a ] and grab that, then we match it to a status depending on which char is inside
+//     let openbracket = input.chars().position(|c| c == '[').unwrap();
+//     // check if there is a closing bracket exactly 2 chars after the opening bracket
+//     let closedbracket = input.chars().position(|c| c == ']').unwrap();
+//     if closedbracket - openbracket == 2 {
+//         status = match input.chars().nth(openbracket + 1).unwrap() {
+//             'x' => Status::DONE,
+//             ' ' => Status::OPEN,
+//             '>' => Status::INPROGRESS,
+//             '!' => Status::OVERDUE,
+//             _ => panic!("Failed to parse Task: Invalid status"),
+//         };
+//     } else {
+//         panic!("Failed to parse Task: Invalid status");
+//     }
+//     // obtaining the title is a bit more tricky, we need to find the first space after the closing bracket, and then take all characters until we encounter a dash or the string ends
+//     title = input.chars().skip(closedbracket + 1).take_while(|c| c != &'-').collect::<String>().trim().to_string();
 
 
-    let due_prefix_index: Option<usize> = input.find("due: ");
-    let priority_prefix_index: Option<usize> = input.find("priority: ");
-    let tags_prefix_index: Option<usize> = input.find("tags: ");
-    // we want to skip the due: prefix, so we need to add 5 to the index and then use chars().skip().take_while()
-    if due_prefix_index.is_some() {
-        due_date = Some(input.chars().skip(due_prefix_index.unwrap() + 5).take_while(|c| c != &' ').collect::<String>());
-    }
-    if priority_prefix_index.is_some() {
-        priority = Some(input.chars().skip(priority_prefix_index.unwrap() + 10).take_while(|c| c != &' ').collect::<String>().parse::<u8>().unwrap());
-    }
-    if tags_prefix_index.is_some() {
-        tags = Some(input.chars().skip(tags_prefix_index.unwrap() + 6).take_while(|c| c != &' ').collect::<String>().split(",").map(|s| s.to_string()).collect::<Vec<String>>());
-    }
-    Some(Task {
-        id,
-        title,
-        status,
-        due_date,
-        priority,
-        tags,
-    })
+//     let due_prefix_index: Option<usize> = input.find("due: ");
+//     let priority_prefix_index: Option<usize> = input.find("priority: ");
+//     let tags_prefix_index: Option<usize> = input.find("tags: ");
+//     // we want to skip the due: prefix, so we need to add 5 to the index and then use chars().skip().take_while()
+//     if due_prefix_index.is_some() {
+//         due_date = Some(input.chars().skip(due_prefix_index.unwrap() + 5).take_while(|c| c != &' ').collect::<String>());
+//     }
+//     if priority_prefix_index.is_some() {
+//         priority = Some(input.chars().skip(priority_prefix_index.unwrap() + 10).take_while(|c| c != &' ').collect::<String>().parse::<u8>().unwrap());
+//     }
+//     if tags_prefix_index.is_some() {
+//         tags = Some(input.chars().skip(tags_prefix_index.unwrap() + 6).take_while(|c| c != &' ').collect::<String>().split(",").map(|s| s.to_string()).collect::<Vec<String>>());
+//     }
+//     Some(Task {
+//         id,
+//         title,
+//         status,
+//         due_date,
+//         priority,
+//         tags,
+//     })
 
-}
+// }
 
-fn stringify(task: Task) -> String {
-    let mut output = String::new();
-    output.push_str(&task.id.to_string());
-    output.push_str(". [");
-    output.push_str(match task.status {
-        Status::OPEN => " ",
-        Status::INPROGRESS => ">",
-        Status::DONE => "x",
-        Status::OVERDUE => "!",
-    });
-    output.push_str("] ");
-    output.push_str(&task.title);
-    if task.due_date.is_some() {
-        output.push_str(" - due: ");
-        output.push_str(&task.due_date.unwrap());
-    }
-    if task.priority.is_some() {
-        output.push_str(" - priority: ");
-        output.push_str(&task.priority.unwrap().to_string());
-    }
-    if task.tags.is_some() {
-        output.push_str(" - tags: ");
-        output.push_str(&task.tags.unwrap().join(","));
-    }
-    return output;
-}
+// fn stringify(task: Task) -> String {
+//     let mut output = String::new();
+//     output.push_str(&task.id.to_string());
+//     output.push_str(". [");
+//     output.push_str(match task.status {
+//         Status::OPEN => " ",
+//         Status::INPROGRESS => ">",
+//         Status::DONE => "x",
+//         Status::OVERDUE => "!",
+//     });
+//     output.push_str("] ");
+//     output.push_str(&task.title);
+//     if task.due_date.is_some() {
+//         output.push_str(" - due: ");
+//         output.push_str(&task.due_date.unwrap());
+//     }
+//     if task.priority.is_some() {
+//         output.push_str(" - priority: ");
+//         output.push_str(&task.priority.unwrap().to_string());
+//     }
+//     if task.tags.is_some() {
+//         output.push_str(" - tags: ");
+//         output.push_str(&task.tags.unwrap().join(","));
+//     }
+//     return output;
+// }
 
-impl Task {
-    pub fn parse (input: &str) -> Option<Task> {
-        parse_from_string(input)
-    }
-    pub fn stringify (task: Task) -> String {
-        stringify(task)
-    }
+// impl Task {
+//     pub fn parse (input: &str) -> Option<Task> {
+//         parse_from_string(input)
+//     }
+//     pub fn stringify (task: Task) -> String {
+//         stringify(task)
+//     }
     // pub fn get_tags (&self) -> Option<Vec<String>> {
     //     self.tags.clone()
     // }
@@ -195,4 +195,4 @@ impl Task {
     //         self.tags = Some(tags);
     //     }
     // }
-}
+//}
