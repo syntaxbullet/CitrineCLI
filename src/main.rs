65dcc9c6d6fc0f7aa742c5e4a99e3a9836e56caf
syntaxbullet <syntaxbullet@protonmail.com>
2024-01-09 18:42:52 +0100

    fix: fix variable name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod task;
 fn main() {
     let t = task::Task::from_string("1. [>] \"Task 1\"; due: 2020-01-21T00:00; priority: 1; tags: tag1, tag2".to_string()).unwrap();
-    let isValid = t.validate();
-    println!("{}", isValid);
+    let is_valid = t.validate();
+    println!("{}", is_valid);
     println!("{}", t);
 }
